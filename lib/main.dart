f1de5340570c49622335dcdaf01d6783d887cee1
František Křížek <fkrizekk@gmail.com>
2024-05-21 17:07:47 +0200

    dasdasd

--- lib/main.dart
+++ lib/main.dart
@@ -260,7 +260,7 @@ class _QuizState extends State<Quiz> {
                           });
                         },
                       );
-                    }).toList(),
+                    }),
                   ],
                 ),
               ),
