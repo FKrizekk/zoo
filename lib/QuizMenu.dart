24a83dce1a60981a6f214ba0f6110b534d9e44d9
flevic <filip.levic@email.cz>
2024-05-21 16:55:35 +0200

    added container list to quizmenu

--- lib/QuizMenu.dart
+++ lib/QuizMenu.dart
@@ -7,6 +7,7 @@ class NewPage extends StatelessWidget {
     final Color colorOrange = const Color.fromARGB(255, 235, 118, 34);
 
     return Scaffold(
+      backgroundColor: colorOrange,
       appBar: AppBar(
         title: Text(
           'Quiz Menu',
@@ -18,21 +19,31 @@ class NewPage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
       ),
-      body:  ListView(
-          children: [
-            Wrap(
-              spacing: 20,
-              runSpacing: 20,
-              children: [
-                Container(
-                  color: const Color.fromARGB(255, 255, 0, 0),
-                  height: 40,
-                  width: 40,
+      body:  Padding(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+            children: [
+              Center(
+                child: Wrap(
+                  spacing: 20,
+                  runSpacing: 20,
+                  children: [
+                    for (var i = 0; i < 15; i++) Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.white
+                      ),
+                      height: 150,
+                      width: 150,
+                    ),
+                    
+        
+                  ],
                 ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+      ),
     );
   }
 }
