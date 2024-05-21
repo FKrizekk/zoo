bd6fc69504f20dcb88757a483061db5f69633fd2
flevic <filip.levic@email.cz>
2024-05-21 16:41:14 +0200

    UseQuizMenudart Davevariors

--- lib/QuizMenu.dart
+++ lib/QuizMenu.dart
@@ -18,21 +18,21 @@ class NewPage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
       ),
-      body: Container(
-        color: colorOrange,
-        padding: EdgeInsets.all(20), // Add some padding for better readability
-        child: Center(
-          child: Text(
-            'You have navigated to the new page!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 24, // Adjust the font size as needed
-              color: Colors.black,
-              fontFamily: 'News Gothic', // Use the same font family as MyApp
+      body:  ListView(
+          children: [
+            Wrap(
+              spacing: 20,
+              runSpacing: 20,
+              children: [
+                Container(
+                  color: const Color.fromARGB(255, 255, 0, 0),
+                  height: 40,
+                  width: 40,
+                ),
+              ],
             ),
-          ),
+          ],
         ),
-      ),
     );
   }
 }
