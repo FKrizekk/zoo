2237bfb2b739bc1767c77830193d4ad3475b1755
HWSxDejv <davedinox@gmail.com>
2024-05-21 17:10:10 +0200

    Syntax fixes

--- lib/quiz_menu.dart
+++ lib/quiz_menu.dart
@@ -2,14 +2,17 @@
 import 'package:flutter/material.dart';
 
 class NewPage extends StatelessWidget {
+  const NewPage({super.key});
+
+  final Color colorOrange = const Color.fromARGB(255, 235, 118, 34);
+  final int numberOfQuizzes = 3;
+
   @override
   Widget build(BuildContext context) {
-    final Color colorOrange = const Color.fromARGB(255, 235, 118, 34);
-
     return Scaffold(
       backgroundColor: colorOrange,
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Quiz Menu',
           style: TextStyle(
             fontSize: 30, // Adjust the font size as needed
@@ -19,30 +22,28 @@ class NewPage extends StatelessWidget {
         ),
         backgroundColor: Colors.white,
       ),
-      body:  Padding(
+      body: Padding(
         padding: const EdgeInsets.all(20),
         child: ListView(
-            children: [
-              Center(
-                child: Wrap(
-                  spacing: 20,
-                  runSpacing: 20,
-                  children: [
-                    for (var i = 0; i < 15; i++) Container(
+          children: [
+            Center(
+              child: Wrap(
+                spacing: 20,
+                runSpacing: 20,
+                children: [
+                  for (var i = 0; i < numberOfQuizzes; i++)
+                    Container(
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.white
-                      ),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white),
                       height: 150,
                       width: 150,
                     ),
-                    
-        
-                  ],
-                ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
     );
   }
